abda4b8fb140b2a676f320e8355a6d179f9958f0
Rohi <105811202+gneiru@users.noreply.github.com>
2022-11-09 07:28:28 +0800

    flx

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,15 +14,16 @@ defineProps({
 </script>
 
 <template>
-  <div class="text-3xl">gneiru</div>
+  <div class="text-3xl text-blue-500 font-black italic">gneiru</div>
   <div class="text-lg">Welcome to my website</div>
-  <button class="button" onclick="location.href='https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2Fgneiru%2Fportfolio%2Fmain%2Fsrc%2Fassets%2Fresume.docx&wdOrigin=BROWSELINK';">Download Resume</button>
   <div class="flex flex-col ">
-    <div class="bg-[#3E4451] m-4 p-2 rounded-lg shadow-lg shadow-cyan-500/50">
+    <div class="bg-[#3E4451] m-4 p-2 rounded-lg shadow-lg shadow-slate-900">
       <div class="text-3xl text-yellow-400">About Me</div>
-      <p class="">I'm {{name}}, a developer from Pangasinan, 23 years old, and an open-source enthusiast</p>
+      <p class="m-2">I'm {{name}}, a developer from Pangasinan, 23 years old, and an open-source enthusiast</p>
+    <button class="button" onclick="location.href='https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2Fgneiru%2Fportfolio%2Fmain%2Fsrc%2Fassets%2Fresume.docx&wdOrigin=BROWSELINK';">Resume</button>
+
     </div>
-    <div class="bg-[#3E4451] m-4 p-2 rounded-lg shadow-lg shadow-cyan-500/50">
+    <div class="bg-[#3E4451] m-4 p-2 rounded-lg shadow-lg shadow-slate-900">
       <div class="text-3xl text-yellow-400">Languages & technologies</div>
       <div class="m-2">
         JavaScript
@@ -38,64 +39,64 @@ defineProps({
         TailwindCSS
       </div>
     </div>
-    <div class="bg-[#3E4451] m-4 p-2 rounded-lg shadow-lg shadow-cyan-500/50">
-      <div class="text-3xl text-yellow-400">Projects</div>
-      <div class="grid grid-cols-2">
-      <span class="bg-[#3C4049] m-5 shadow-inner shadow-cyan-500/50 rounded-lg">
-        <div class="text-lg ">Personal Health Care Guide</div>
-        <a href="http://youtube.com/watch?v=_PT9XjHJU2M" target="_blank" rel="noopener noreferrer">
-          <img :src="health" alt="" class="p-2">
-        </a>
-      </span>
-      <span class="bg-[#3C4049] m-5 shadow-inner shadow-cyan-500/50 rounded-lg">
-        <div class="text-lg ">Child Growth Monitoring and Malnutrition System</div>
-        <a :href="cgs" target="_blank" rel="noopener noreferrer">
-          <img :src="cgs" alt="" class="p-2">
-        </a>
-      </span>
-      <span class="bg-[#3C4049] m-5 shadow-inner shadow-cyan-500/50 rounded-lg">
-        <div class="text-lg">Bounty Experience Calculator</div>
-        <a href="https://gneiru.github.io/vite-exp-calculator/" target="_blank" rel="noopener noreferrer">
-          <img :src="calc" alt="" class="p-2">
-        </a>
-      </span >
-      <span class="bg-[#3C4049] m-5 shadow-inner shadow-cyan-500/50 rounded-lg">
-        <div class="text-lg">Raid Map Generator</div>
-        <a href="https://gneiru.github.io/raid-maps/" target="_blank" rel="noopener noreferrer">
-          <img :src="raidmap" alt="" class="p-2">
-        </a>
-      </span>
-      <span class="bg-[#3C4049] m-5 shadow-inner shadow-cyan-500/50 rounded-lg">
-        <div class="text-lg">Guardian Tales Heroes</div>
-        <a href="https://gneiru.github.io/gt-search/" target="_blank" rel="noopener noreferrer">
-          <img :src="gt" alt="" class="p-2">
-        </a>
-      </span>
-      <span class="bg-[#3C4049] m-5 shadow-inner shadow-cyan-500/50 rounded-lg">
-        <div class="text-lg">Orca Discord Bot</div>
-        <a href="https://shidou-bot.github.io/" target="_blank" rel="noopener noreferrer">
-          <img :src="orca" alt="" class="p-2">
-        </a>
-      </span>
-      </div>
-      <div class="bg-[#3E4451] m-4 p-2 rounded-lg shadow-lg shadow-cyan-500/50">
-        <div class="text-3xl text-yellow-400 mb-4">Socials</div>
-        <div class="m-auto">
-          <a href="https://facebook.com/noel.rohi" class="m-2 text-black hover:text-white">Facebook</a>
-          <a href="https://linkedin.com/in/gneiru" class="m-2 text-black hover:text-white">LinkedIn</a>
-          <a href="https://github.com/gneiru" class="m-2 text-black hover:text-white">Github</a>
-          <a href="https://instagram.com/nelrohg" class="m-2 text-black hover:text-white">Instagram</a>
-        </div>
+    <div class="bg-[#3E4451] m-4 p-2 rounded-lg shadow-lg shadow-slate-900">
+      <div class="text-3xl text-yellow-400 m-4">Projects</div>
+      <div class="flex flex-wrap">
+        <span class="bg-[#3C4049] shadow-inner shadow-gray-900 rounded-lg lg:w-1/2">
+          <div class="text-lg ">Personal Health Care Guide</div>
+          <a href="http://youtube.com/watch?v=_PT9XjHJU2M" target="_blank" rel="noopener noreferrer">
+            <img :src="health" alt="" class="p-2">
+          </a>
+        </span>
+        <span class="bg-[#3C4049] shadow-inner shadow-gray-900 rounded-lg lg:w-1/2">
+          <div class="text-lg ">Child Growth Monitoring and Malnutrition System</div>
+          <a :href="cgs" target="_blank" rel="noopener noreferrer">
+            <img :src="cgs" alt="" class="p-2">
+          </a>
+        </span>
+        <span class="bg-[#3C4049] shadow-inner shadow-gray-900 rounded-lg lg:w-1/2">
+          <div class="text-lg">Bounty Experience Calculator</div>
+          <a href="https://gneiru.github.io/vite-exp-calculator/" target="_blank" rel="noopener noreferrer">
+            <img :src="calc" alt="" class="p-2">
+          </a>
+        </span >
+        <span class="bg-[#3C4049] shadow-inner shadow-gray-900 rounded-lg lg:w-1/2">
+          <div class="text-lg">Raid Map Generator</div>
+          <a href="https://gneiru.github.io/raid-maps/" target="_blank" rel="noopener noreferrer">
+            <img :src="raidmap" alt="" class="p-2">
+          </a>
+        </span>
+        <span class="bg-[#3C4049] shadow-inner shadow-gray-900 rounded-lg lg:w-1/2">
+          <div class="text-lg">Guardian Tales Heroes</div>
+          <a href="https://gneiru.github.io/gt-search/" target="_blank" rel="noopener noreferrer">
+            <img :src="gt" alt="" class="p-2">
+          </a>
+        </span>
+        <span class="bg-[#3C4049] p-4 shadow-inner shadow-gray-900 rounded-lg lg:w-1/2">
+          <div class="text-lg">Orca Discord Bot</div>
+          <a href="https://shidou-bot.github.io/" target="_blank" rel="noopener noreferrer">
+            <img :src="orca" alt="" class="p-2">
+          </a>
+        </span>
       </div>
       
-
+      </div>
+      
+  <div class="bg-[#3E4451] m-4 p-2 rounded-lg shadow-lg shadow-slate-900 break-words">
+    <div class="text-3xl text-yellow-400 mb-4">Socials</div>
+    <div>
+      <a href="https://facebook.com/noel.rohi" class="m-2 text-black hover:text-white">Facebook</a>
+      <a href="https://linkedin.com/in/gneiru" class="m-2 text-black hover:text-white">LinkedIn</a>
+      <a href="https://github.com/gneiru" class="m-2 text-black hover:text-white">Github</a>
+      <a href="https://instagram.com/nelrohg" class="m-2 text-black hover:text-white">Instagram</a>
     </div>
   </div>
+</div>
 </template>
 
 <style>
 .button {
-  border-color: blueviolet;
+  border-color: #3E4451;
   background-color: #1A1A1A;
   padding: 10px;
  }
